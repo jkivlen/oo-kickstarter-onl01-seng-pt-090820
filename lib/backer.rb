58c7601e07ba74b9278c05077bf3a344ce6e7128
Jimmy Kivlen <jimmykivlen@gmail.com>
2020-10-11 01:27:01 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -3,11 +3,13 @@ class Backer
   attr_reader :name, :backed_projects
   
   def initialize(name)
-    
     @name = name
     @backed_projects = []
   end
   
-  
+  def back_project
+    
+    
+  end
   
 end
